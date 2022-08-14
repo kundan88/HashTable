a20aa37f5e56ec9d98e719c8725d69325ef9f3ef
Kundan <kundan kamble>
2022-08-14 01:58:39 -0800

    frequency Of Words In a sentance

---
+++ HashTable/LinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class LinkedList<K, V> where K : IComparable
+    {
+        //Linked list Node created with key-value pair
+        public MyMapNode<K, V> head;
+        public MyMapNode<K, V> tail;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+        public MyMapNode<K, V> Search(K key)
+        {
+            //Head will assign as temp
+            MyMapNode<K, V> temp = head;
+            while (temp != null)
+            {
+                if (temp.key.Equals(key))
+                    return temp;
+                temp = temp.next;
+            }
+            return null;
+        }
+        public void Append(MyMapNode<K, V> node)
+        {
+            //Check node is present or not
+            if (head == null && tail == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                //If present then node added from the End
+                tail.next = node;
+                tail = node;
+            }
+        }
+    }
+}
+
---
+++ HashTable/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1:Find the frequency of word in sentence
+            Console.WriteLine("Frequency of Word");
+            LinkedHashMap<string, int> linkedHashMap = new LinkedHashMap<string, int>(5);
+            string sentence = "to be or not to be";
+            string[] words = sentence.ToLower().Split(" ");
+            foreach (string word in words)
+            {
+                int value = linkedHashMap.Get(word);
+                if (value == default)
+                {
+                    value = 1;
+                }
+                else value += 1;
+                linkedHashMap.Add(word, value);
+            }
+            int frequency = linkedHashMap.Get("be");
+            Console.WriteLine(frequency);
+        }
+    }
+}
+
