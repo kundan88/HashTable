8f4f45a4a498a2d84a6ff119dd6e2dc30266c1fb
Kundan <kundan kamble>
2022-08-14 02:03:23 -0800

    Find Large frequency Of Words In a Paragraph

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -4,24 +4,23 @@
     {
         static void Main(string[] args)
         {
-            //UC1:Find the frequency of word in sentence
-            Console.WriteLine("Frequency of Word");
-            LinkedHashMap<string, int> linkedHashMap = new LinkedHashMap<string, int>(5);
-            string sentence = "to be or not to be";
-            string[] words = sentence.ToLower().Split(" ");
-            foreach (string word in words)
-            {
-                int value = linkedHashMap.Get(word);
-                if (value == default)
+                Console.WriteLine("Welcome to the HashTable");
+                LinkedHashMap<string, int> linkedHashMap = new LinkedHashMap<string, int>(5);
+                string sentence = "paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+                string[] words = sentence.ToLower().Split(" ");
+                foreach (string word in words)
                 {
-                    value = 1;
+                    int value = linkedHashMap.Get(word);
+                    if (value == default)
+                    {
+                        value = 1;
+                    }
+                    else value += 1;
+                    linkedHashMap.Add(word, value);
                 }
-                else value += 1;
-                linkedHashMap.Add(word, value);
+                int frequency = linkedHashMap.Get("paranoid");
+                Console.WriteLine(frequency);
             }
-            int frequency = linkedHashMap.Get("be");
-            Console.WriteLine(frequency);
         }
-    }
 }
 
